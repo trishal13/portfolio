96aaeaf8c251c26982423269e6fd65e1d016693a
Trishal <trishalmakhija@gmail.com>
2025-01-07 21:52:06 +0530

    changes

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -88,7 +88,7 @@ const Slideshow = () => {
                         <span
                             key={idx}
                             style={{ fontFamily: 'Consolas, monospace' }}
-                            className="bg-gradient-to-r from-red-700 to-red-500 text-white px-4 py-2 rounded-full text-sm font-semibold shadow-lg"
+                            className="bg-red-700 text-white px-4 py-2 rounded-full text-sm font-semibold shadow-lg"
                         >
                             {skill}
                         </span>
